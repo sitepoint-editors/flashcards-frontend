f7d3f1a18d4b553ec130615483c131b8db614fb1
Ivaylo Gerchev <biz.mailbox@abv.bg>
2020-10-13 18:39:13 +0300

    deploy

---
+++ src/components/views/AddFlashcard.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { toast } from 'react-toastify';
+
+const ADD_FLASHCARD = gql`
+  mutation createFlashcard($question: String!, $answer: String!) {
+    createFlashcard(input: { question: $question, answer: $answer }) {
+      _id
+      question
+      answer
+    }
+  }
+`;
+
+const FLASHCARDS_QUERY = gql`
+  {
+    allFlashcards {
+      _id
+      question
+      answer
+    }
+  }
+`;
+
+const AddFlashcard = ({ history }) => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const [createFlashcard] = useMutation(ADD_FLASHCARD, {
+    update(
+      cache,
+      {
+        data: { createFlashcard }
+      }
+    ) {
+      const { allFlashcards } = cache.readQuery({ query: FLASHCARDS_QUERY });
+
+      cache.writeQuery({
+        query: FLASHCARDS_QUERY,
+        data: { allFlashcards: [...allFlashcards, createFlashcard] }
+      });
+    },
+    onCompleted() { history.push(`/`) }
+  });
+
+  return (
+    <div className="container mt-3">
+      <h1 className="title">Add Flashcard</h1>
+
+      <div className="box mt-3">
+        <form onSubmit={e => {
+          e.preventDefault();
+          createFlashcard({
+            variables: {
+              question,
+              answer
+            }
+          });
+          toast.success("Flashcard was created successfully!", {
+            position: toast.POSITION.TOP_CENTER
+          });
+        }}>
+
+          <div className="field">
+            <label className="label">Question</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="question"
+                value={question}
+                onChange={e => setQuestion(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Answer</label>
+            <div className="control">
+              <textarea
+                className="textarea"
+                name="answer"
+                rows="5"
+                value={answer}
+                onChange={e => setAnswer(e.target.value)}
+                required
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <button className="button is-link">Add</button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddFlashcard;
---
+++ src/components/views/AllFlashcards.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import Flashcard from "../Flashcard"
+
+const FLASHCARDS_QUERY = gql`
+  {
+    allFlashcards {
+      _id
+      question
+      answer
+      isAnswered
+    }
+  }
+`;
+
+const DELETE_FLASHCARD_QUERY = gql`
+  mutation deleteFlashcard($_id: ID!) {
+    deleteFlashcard(_id: $_id) {
+      _id
+    }
+  }
+`;
+
+const EDIT_FLASHCARD = gql`
+  mutation updateFlashcard($_id: ID!, $isAnswered: Boolean) {
+    updateFlashcard(_id: $_id, input: { isAnswered: $isAnswered }) {
+      _id
+      isAnswered
+    }
+  }
+`;
+
+const AllFlashcards = () => {
+  const { loading, error, data } = useQuery(FLASHCARDS_QUERY);
+
+  const [deleteFlashcard] = useMutation(DELETE_FLASHCARD_QUERY, {
+    update(
+      cache,
+      {
+        data: { deleteFlashcard }
+      }
+    ) {
+      const { allFlashcards } = cache.readQuery({ query: FLASHCARDS_QUERY });
+      const updatedFlashcards = allFlashcards.filter(flashcard => flashcard._id !== deleteFlashcard._id);
+
+      cache.writeQuery({
+        query: FLASHCARDS_QUERY,
+        data: { allFlashcards: updatedFlashcards }
+      }); 
+    }
+  });
+
+  const [updateFlashcard] = useMutation(EDIT_FLASHCARD);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div className="container mt-3">
+      <h1 className="title">All Flashcards
+        <button className="button is-info is-light ml-3" onClick={ () => {
+          data.allFlashcards.map( function(flashcard) { 
+            if (flashcard.isAnswered) {
+              updateFlashcard({
+                variables: {
+                  _id: flashcard._id,
+                  isAnswered: false
+                }
+              });
+            }
+          });
+        }}>
+          Reset Flashcards
+        </button>
+      </h1>
+
+      <div className="box has-background-info">
+        <div className="columns is-multiline">
+          { data.allFlashcards.map( function(flashcard) { 
+              if (!flashcard.isAnswered) {
+                return (  
+                  <div className="column is-one-third" key={flashcard._id}>
+                    <Flashcard flashcard={flashcard} deleteFlashcard={deleteFlashcard} />
+                  </div>
+                )
+              }          
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllFlashcards;
